a39ec248ea2c1da366e6c078f087aff017a78497
amr05645 <amrdawoud702@gmail.com>
2022-02-18 11:22:20 +0200

    prepare project for work

--- TaskForSoftXpert/Scenes/Home Scene/HomeVC.swift
+++ TaskForSoftXpert/Scenes/Home Scene/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  TaskForSoftXpert
 //
 //  Created by Amr on 2/18/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
